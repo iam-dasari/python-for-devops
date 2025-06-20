fa1ec4f0163af2b01f0fd806f73260eccb60c61a
Bullaiah <bullaiahd@gmail.com>
2025-06-20 12:37:06 +0530

    Variables, Values & Type-casting

--- 03-variables.py
+++ 03-variables.py
@@ -11,3 +11,10 @@ name='Dasari'
 
 print(a)
 print(name)
+
+# Type casting
+x = str(3)    # x will be '3'
+y = int(3)    # y will be 3
+z = float(3)  # z will be 3.0
+
+print("x = ",x,"\ny = ",y,"\nz = ",z)
