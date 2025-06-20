518cf4fcf7e7826dda9f8f98be35f6a02a251e2b
Bullaiah <bullaiahd@gmail.com>
2025-06-20 12:33:03 +0530

    Variables

---
+++ 03-variables.py
@@ -0,0 +1,13 @@
+"""
+This is a comment
+written in
+more than just one line
+"""
+# Single line comment
+
+# Below a and name are varibles which have values defined
+a=10
+name='Dasari'
+
+print(a)
+print(name)
