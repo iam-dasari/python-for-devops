8c1afd3f0be1233fdc2791a5a8ac657284c40def
Bullaiah <bullaiahd@gmail.com>
2025-06-20 16:02:18 +0530

    Upload a file to AWS S3

--- python-boto3-tasks/01-upload-file-s3.py
+++ python-boto3-tasks/01-upload-file-s3.py
@@ -3,3 +3,5 @@ import boto3
 s3 = boto3.client('s3')
 
 s3.upload_file('hello.txt', 'bucket-name', 'hello.txt') #Replace bucket-name with your AWS S3 bucket name
+
+#ACCESS KEY, SECRET ACCESS KEY, REGION can be entered using "aws configure"
